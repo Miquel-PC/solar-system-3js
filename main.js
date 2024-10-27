6aead1c89974d5686d67bd15aebf1459703aeb9c
Miquel-PC <mplanissi@gmail.com>
2024-10-27 22:01:34 +0100

    Refactoring & position changed for earth 2

--- main.js
+++ main.js
@@ -5,35 +5,41 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 const width = window.innerWidth;
 const height = window.innerHeight;
 
+// scene
 const renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
 const loader = new GLTFLoader();
 
-
-renderer.shadowMap.enabled = true;
-const camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
-const scene = new THREE.Scene();
-
+const camera = new THREE.PerspectiveCamera(90, width/height, 0.1, 3000);
 const light = new THREE.DirectionalLight(0xffffff, 10);
 const ambient = new THREE.AmbientLight(0xf04040, 1);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
 let time = Date.now();
+
+renderer.setSize(width, height);
+renderer.shadowMap.enabled = true;
+light.castShadow = true;
 
 scene.add(ambient);
 scene.add(light);
-light.castShadow = true;
 
+camera.translateZ(700);
+camera.translateY(250);
 
-// CAMERA
-camera.translateZ(150);
-camera.translateY(50);
+document.querySelector("body").appendChild(renderer.domElement);
 
 
 // object declaration & initialization
-const systemObjects = [];
+const systemObjects = []; // unused
 
 const solarSystem = new THREE.Object3D();
+
 const earthOrbit = new THREE.Object3D();
 const earthOrbit2 = new THREE.Object3D();
+
 const moonOrbit = new THREE.Object3D();
 const moonOrbit2 = new THREE.Object3D();
 const moonOrbit3 = new THREE.Object3D();
@@ -47,23 +53,17 @@ systemObjects.push(moonOrbit);
 solarSystem.add(earthOrbit);
 solarSystem.add(earthOrbit2);
 
-earthOrbit.position.x = 200;
-earthOrbit.position.y = 30;
+earthOrbit.position.set(150, 30, 0);
 earthOrbit.add(moonOrbit);
 moonOrbit.position.x = 30;
 
-
-earthOrbit2.position.x = 400;
-earthOrbit2.position.y = 30;
+earthOrbit2.position.set(80, 30, 300);
 earthOrbit2.add(moonOrbit2);
 earthOrbit2.add(moonOrbit3);
-moonOrbit2.position.x = 50;
-moonOrbit3.position.x = 90;
-moonOrbit3.position.z = 30;
-
+moonOrbit2.position.x = 30;
+moonOrbit3.position.set(50, 0, 50);
 
 scene.add(solarSystem);
-
 
 
 // texture loading
@@ -109,6 +109,7 @@ objectLoader(
     moonOrbit3
 );
 
+renderer.setAnimationLoop(animate);
 
 function objectLoader(url, size, position, orbit) {
     loader.load(url, (gltf) => {
@@ -120,21 +121,18 @@ function objectLoader(url, size, position, orbit) {
     });
 }
 
-
-renderer.setSize(width, height);
-renderer.setAnimationLoop(animate);
-
-document.querySelector("body").appendChild(renderer.domElement);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
 function animate() {
     const currentTime = Date.now();
     const deltaTime = currentTime - time;
     time = currentTime;
 
     solarSystem.rotation.y += 0.001 * deltaTime;
-    earthOrbit.rotation.y += 0.002 * deltaTime;
+    earthOrbit.rotation.y += 0.004 * deltaTime;
     moonOrbit.rotation.y += 0.003 * deltaTime;
+
+    earthOrbit2.rotation.y += 0.001 * deltaTime;
+    moonOrbit2.rotation.y += 0.004 * deltaTime;
+    moonOrbit3.rotation.y += 0.003 * deltaTime;
+
     renderer.render(scene, camera);
 }
